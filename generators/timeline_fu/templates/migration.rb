2e9a1ed49a79521b7e3e0d864987dc122dd82e2d
jphillips <jphillips@jphillips.local>
2010-10-20 18:23:29 -0400

    added message and url to timeline

--- generators/timeline_fu/templates/migration.rb
+++ generators/timeline_fu/templates/migration.rb
@@ -1,7 +1,8 @@
 class CreateTimelineEvents < ActiveRecord::Migration
   def self.up
     create_table :timeline_events do |t|
-      t.string   :event_type, :subject_type,  :actor_type,  :secondary_subject_type
+      t.string   :event_type, :subject_type,  :actor_type,  :secondary_subject_type, :url
+      t.text     :message, :default => ""
       t.integer               :subject_id,    :actor_id,    :secondary_subject_id
       t.timestamps
     end
